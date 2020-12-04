1febd3567b1140d22e2b5725ab07cae5b353149b
OszukiwaczPl <75497205+OszukiwaczPl@users.noreply.github.com>
2020-12-04 19:29:29 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+    Public Sub Main(args As String())
+        Console.WriteLine("Install Virus.")
+        Console.WriteLine("Install Virus done. :)")
+        Console.WriteLine("Run Virus!")
+        Console.WriteLine("Usuwanie Antivirusa. :)")
+        Console.WriteLine("Usuwanie Systemu. :)")
+        Console.WriteLine("Reset. :)")
+    End Sub
+End Module
+
