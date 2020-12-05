86120b00a9ea783a777faba881fcdbb5988afcc5
OszukiwaczPl <75497205+OszukiwaczPl@users.noreply.github.com>
2020-12-05 10:41:34 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        NewMethod()
+        Console.WriteLine("Install AntiVirus.")
+    End Sub
+
+    Private Sub NewMethod()
+        Console.WriteLine("Install System.")
+        Console.WriteLine("Install Wi-Fi.")
+        Console.WriteLine("Install Done System.")
+    End Sub
+End Module
